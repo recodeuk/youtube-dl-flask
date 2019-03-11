c21288356c4a792b1a34c3aafc8fbaffbda7a014
tfyl <ttttufayl@gmail.com>
2019-03-11 01:20:44 +0000

    fixes

--- static/countdown.js
+++ static/countdown.js
@@ -1,3 +1,4 @@
+/*
 newFunction();
 function newFunction() {
   var countdownNumberEl = document.getElementById('countdown-number');
@@ -8,3 +9,24 @@ function newFunction() {
     countdownNumberEl.textContent = countdown;
   }, 1000);
 }
+*/
+
+var seconds = 10;
+      
+function countdown() {
+    seconds = seconds - 1;
+    if (seconds < 0) {
+        var Url = window.location;
+        // Redirection link 
+        window.location = Url["origin"] + "/youtube-dl" ;
+    } else {
+        // Update remaining seconds
+        document.getElementById("countdown-number").innerHTML = seconds;
+        console.log(seconds)
+        // Count down using javascript
+        window.setTimeout("countdown()", 1000);
+    }
+}
+
+// Run countdown function
+countdown();
