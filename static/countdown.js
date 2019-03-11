a07d2484ceb7890696dc6bdc3d76853d6b3444aa
tfyl <ttttufayl@gmail.com>
2019-03-11 01:00:03 +0000

    changes

--- static/countdown.js
+++ static/countdown.js
@@ -1,10 +1,10 @@
-var countdownNumberEl = document.getElementById('countdown-number');
-var countdown = 5;
-
-countdownNumberEl.textContent = countdown;
-
-setInterval(function() {
-  countdown = --countdown <= 0 ? 5 : countdown;
-
+newFunction();
+function newFunction() {
+  var countdownNumberEl = document.getElementById('countdown-number');
+  var countdown = 5;
   countdownNumberEl.textContent = countdown;
-}, 1000);
+  setInterval(function () {
+    countdown = --countdown <= 0 ? 5 : countdown;
+    countdownNumberEl.textContent = countdown;
+  }, 1000);
+}
